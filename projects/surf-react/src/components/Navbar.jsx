6f517e299fd901e1f0baf482293123334c1701c7
danieldiaz98 <danieldiaz.g98@gmail.com>
2025-03-26 11:31:28 +0000

    change navbar style

--- projects/surf-react/src/components/Navbar.jsx
+++ projects/surf-react/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import './Navbar.css'
 
 function Navbar () {
     return (
@@ -20,10 +20,8 @@ function Navbar () {
                 <a className="nav-link" href="#">Spots</a>
                 </li>
             </ul>
-            <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <a href="#">Inicia Sesión</a>
+            <a href="#">Registrate</a>
             </div>
         </div>
         </nav>
