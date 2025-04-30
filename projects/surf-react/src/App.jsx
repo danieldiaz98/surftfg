e3ce2e092f5386511d254e5d1cea715e2d75ffd7
danieldiaz98 <danieldiaz.g98@gmail.com>
2025-04-30 11:33:33 +0100

    feat: showing database info through component

--- projects/surf-react/src/App.jsx
+++ projects/surf-react/src/App.jsx
@@ -5,15 +5,34 @@ import SpotCard from "./components/SpotCard";
 import "./App.css"
 import { client } from "./supabase/client";
 import Spotsdb from "./supabase/Spotsdb";
+import { getAllSpots } from "./supabase/spotServices";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [spots, setSpots] = useState([]);
+    useEffect (() => {
+      async function fetchSpots() {
+        const spotsData = await getAllSpots();
+        setSpots(spotsData);
+      }
+      fetchSpots();
+    }, [])
   return (
-    
     <>
       <Navbar/>
       <h2>Surf spots populares</h2>
-      <SpotCard/>
       <Spotsdb/>
+      <div className="spot_cards_container">
+        {spots.map((spot) => (
+          <SpotCard
+            key={spot.id}
+            name={spot.Name}
+            location={spot.Location}
+            imageUrl={spot.image_url}
+          />
+        ))}
+
+      </div>
     </>
   )
 }
--- projects/surf-react/src/components/SpotCard.jsx
+++ projects/surf-react/src/components/SpotCard.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-function SpotCard () {
+function SpotCard ({name, location, imageUrl}) {
     return (
         <div className="card" style={{ width: "18rem" }}>
-            <img src="/lascanteras.jpg" className="card-img-top" alt="beach photo" />
+            <img src={imageUrl} className="card-img-top" alt="beach photo" />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">{name}</h5>
             </div>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item">Nombre</li>
-                <li className="list-group-item">Localización</li>
+                <li className="list-group-item">{location}</li>
             </ul>
             <div className="card-body">
                 <a href="#" className="card-link">Card link</a>
