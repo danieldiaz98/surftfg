68788ee37bf958d9d2a09a7bc2e07847d9413a39
danieldiaz98 <danieldiaz.g98@gmail.com>
2025-05-02 10:09:56 +0100

    style: spot card style changed

--- projects/surf-react/src/components/SpotCard.jsx
+++ projects/surf-react/src/components/SpotCard.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function SpotCard ({name, location, imageUrl}) {
     return (
-        <div className="card" style={{ width: "18rem", display: "inline-block", margin: "10px" }}>
-            <img src={imageUrl} className="card-img-top" alt="beach photo" />
+        <div className="card" style={{ display: "inline-block", margin: "10px" }}>
+            <img src={imageUrl} className="card-img-top" alt="beach photo" 
+            style={{width: "auto", height: "300px"}}/>
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
             </div>
