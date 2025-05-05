3fe29812c50bf722b673aa2d9e05e0208feaf188
danieldiaz98 <danieldiaz.g98@gmail.com>
2025-05-05 12:16:39 +0100

    feat: SpotsMap component created

---
+++ projects/surf-react/src/components/SpotsMap.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function SpotsMap() {
+    return (
+        <>
+            <Navbar />
+            <div>
+                <h1>Mapa de Spots</h1>
+                <p>Este es el mapa de spots.</p>
+            </div>
+        </>
+    );
+}
+export default SpotsMap;
