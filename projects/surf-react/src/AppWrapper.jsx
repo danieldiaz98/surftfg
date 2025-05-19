80a2955f710e5cf529fe37941022d508cd5324cf
danieldiaz98 <danieldiaz.g98@gmail.com>
2025-05-19 13:59:33 +0100

    feat: appwrapper

---
+++ projects/surf-react/src/AppWrapper.jsx
@@ -0,0 +1,20 @@
+// src/AppWrapper.jsx
+import { Spinner } from "react-bootstrap";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
+import { UserAuth } from "./context/AuthContext";
+
+export default function AppWrapper() {
+  const { session } = UserAuth();
+
+  if (session === undefined) {
+    // Supabase aún no ha cargado la sesión
+    return (
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <Spinner animation="border" />
+      </div>
+    );
+  }
+
+  return <RouterProvider router={router} />;
+}
