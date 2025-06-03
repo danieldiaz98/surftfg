92217b5a35f9c5c71a39746b504a7c57879fd9a8
danieldiaz98 <danieldiaz.g98@gmail.com>
2025-06-03 11:33:17 +0100

    chore: project set for android devices

---
+++ projects/surf-react/android/app/src/main/java/com/surftfg/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.surftfg.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- projects/surf-react/vite.config.js
+++ projects/surf-react/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // clave para que los recursos carguen en app nativa
+  build: {
+    outDir: 'dist',
+  },
 })
